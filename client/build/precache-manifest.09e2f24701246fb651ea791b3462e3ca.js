f79dc4130eaf37ffa191fb7fd882406f7a09a4a9
Scott <clearplaid@gmail.com>
2019-08-01 16:34:12 -0400

    update build

---
+++ client/build/precache-manifest.09e2f24701246fb651ea791b3462e3ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7418d9b3d7e4b7fea333f91e05f9b12",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0aa08639f4f5a975a9aa",
+    "url": "/static/css/main.8dca69cf.chunk.css"
+  },
+  {
+    "revision": "b6a49f221a6931e81c93",
+    "url": "/static/js/2.eb7a1239.chunk.js"
+  },
+  {
+    "revision": "0aa08639f4f5a975a9aa",
+    "url": "/static/js/main.940102b5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4f926c38e69233161dc456fd331c2471.js"
+  "/precache-manifest.09e2f24701246fb651ea791b3462e3ca.js"
 );
 
 self.addEventListener('message', (event) => {
